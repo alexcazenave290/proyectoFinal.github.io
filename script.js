30e66cfd15083b378820a4ce20c4aeb08588ae54
alexcazenave290 <alexcazenave290@gmail.com>
2025-09-13 22:27:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Inicializar la animación de las patitas al cargar la página
-    iniciarAnimacionPatitas();
-});
 
-function iniciarAnimacionPatitas() {
-    const paws = document.querySelectorAll('.paw-bg');
-    paws.forEach(paw => {
-        animarPatita(paw);
-    });
-}
-
-function animarPatita(paw) {
-    const randomDeg = Math.floor(Math.random() * 91) - 45;
-    paw.style.transform = `rotate(${randomDeg}deg)`;
-    const nextTime = 700 + Math.random() * 1000;
-    setTimeout(() => animarPatita(paw), nextTime);
-}
